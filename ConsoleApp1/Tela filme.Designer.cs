70127109ac1cf3f25b54818fe92aa705f1618c9f
Unifaat <Unifaat@DESKTOP-5F5QE0A>
2022-09-05 22:30:20 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Tela filme.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace ConsoleApp1
+{
+    partial class Tela_filme
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Tela_filme
+            // 
+            this.ClientSize = new System.Drawing.Size(734, 322);
+            this.Name = "Tela_filme";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSinopse;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtGenero;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtClassificacao;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ ConsoleApp1/Tela filme.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    public partial class Tela_filme : Form
+    {
+        public Tela_filme()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        Filme f = new Filme();//instanciando objeto
+        Filme[]vet = new Filme[5];//declaração do vetor de objetos
+        int i = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (i==5)
+            {
+                MessageBox.Show("Não foi possivel gravar!");
+            }
+            else
+            {
+                f.Titulo = txtTitulo.Text;
+                f.Classificacao=txtClassificacao.Text;
+                f.Genero = txtGenero.Text;
+                f.Sinopse = txtSinopse.Text;
+                vet[i] = f;
+                i++;
+            }
+            limpar();
+        }
+        private void limpar()
+        {
+            txtSinopse.Text = null;
+            txtTitulo.Text = null;
+            txtGenero.Text = null;
+            txtClassificacao.Text = null;
+        }
+        private void btnPesquisar_Click(object sender,EventArgs e)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                if (txtTitulo.Text.Equals(vet[x].Titulo))
+                {
+                    txtClassificacao.Text = vet[x].Classificacao;
+                    txtGenero.Text = vet[x].Genero;
+                    txtSinopse.Text = vet[x].Sinopse;
+                    x = 5;
+
+                }
+                else
+                {
+                    if(x==4)
+                    {
+                        MessageBox.Show("Titulo não existe!");
+                    }
+                }
+            }
+        }
+
+       
+    }
+}
